fdf7b7130d7ade48f6fb84cc60df00d32cfe9652
zh384144429 <56762430+zh384144429@users.noreply.github.com>
2025-01-21 16:11:42 +0800

    Update python_flask.sh

--- python_flask.sh
+++ python_flask.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt -y git gcc install python3 python3-pip python3-venv python3-flask
+sudo apt -y install git gcc  python3 python3-pip python3-venv python3-flask
 sudo git clone https://github.com/collinsctk/Azure_K8S_DevOps.git
 cd Azure_K8S_DevOps/
 sudo python3 app.py &
