1425d22e211abebbd98f0128059d13a645a58f15
zh384144429 <56762430+zh384144429@users.noreply.github.com>
2025-01-25 19:30:04 +0800

    Update db_init.sh

--- db_init.sh
+++ db_init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-yum install -y git gcc python3 python3-devel
-yum install -y postgresql*
-pip3 install flask
-pip3 install psycopg2-binary
+sudo apt update
+sudo apt install -y git gcc python3 python3-dev python3-pip
+sudo apt install -y postgresql postgresql-contrib
+sudo apt install -y python3-flask
+sudo apt install python3-psycopg2
